77022c8770e7f7a52d67e1e1e5393dfcffcf0cc4
JKJUN527 <631642753@qq.com>
2018-05-02 14:17:32 +0800

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -171,5 +171,19 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     });
 </script>
 <!-- FlexSlider -->
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
 @section('custom-script')
     @show
